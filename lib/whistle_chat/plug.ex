45bb8db5ee33657dd850d9b8f51a84521c48b05d
Mohamed boudra <boudra.moha@gmail.com>
2019-01-21 10:23:56 +0100

    update chat

--- lib/whistle_chat/plug.ex
+++ lib/whistle_chat/plug.ex
@@ -1,31 +1,20 @@
 defmodule WhistleChat.Plug do
   use Plug.Builder
 
-  defp index_html(conn) do
-    """
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title></title>
-    </head>
-    <body>
-      #{}
-      <script src="/js/whistle.js"></script>
-    </body>
-    </html>
-    """
-  end
+  plug(Plug.Logger)
 
-  plug Plug.Logger
-
-  plug Plug.Static,
+  plug(Plug.Static,
     at: "/",
     from: :whistle_chat,
     gzip: false,
     only: ~w(css js favicon.ico robots.txt)
+  )
 
-  plug :index
+  plug(Whistle.Navigation.Plug,
+    router: WhistleChat.ProgramRouter,
+    program: "counter",
+    params: %{}
+  )
 
   @adjectives ~w(Magical Quiet Happy Hungry)
   @animals ~w(Llama Badger Bear Lion Cat)
@@ -34,16 +23,11 @@ defmodule WhistleChat.Plug do
     user =
       Enum.join([Enum.random(@adjectives), Enum.random(@animals), to_string(:rand.uniform(100))])
 
-    resp =
-      Whistle.Program.fullscreen(
-        conn,
-        WhistleChat.ProgramRouter,
-        "main",
-        %{"user" => user, "path" => conn.path_info}
-      )
-
     conn
-    |> put_resp_content_type("text/html")
-    |> send_resp(200, resp)
+    |> Whistle.Program.fullscreen(
+      WhistleChat.ProgramRouter,
+      "main",
+      %{"user" => user, "path" => conn.path_info}
+    )
   end
 end
